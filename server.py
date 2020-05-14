831f1331f661cd6b7d782b6cb69667286dc18139
jwenck70 <60196651+jwenck70@users.noreply.github.com>
2020-05-13 17:51:03 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 app = Flask(__name__)
 
 
@@ -9,6 +9,16 @@ def hello_world():
 # @app.route('/components.html')
 # def components_page():
 #     return render_template('components.html')
+
+@app.route('/challenge')
+def challenge():
+    return redirect('https://content.e-motionsengineering.com/perfect-life-10-day-meditation-challenge')
+    #return render_template('10daychallenge.html')
+    #return 'We are engineering a better website before we engineer your emotions and energy. Stay tuned! Please :D'
+
+@app.route('/challenge/')
+def challenge2():
+    return redirect('https://content.e-motionsengineering.com/perfect-life-10-day-meditation-challenge')
 
 @app.route('/<string:page_name>')
 def html_page(page_name):
